7b94579ab469ead577aaea42e40bdbcfe097b9de
DimitrisKas <dimitriskas.1997@gmail.com>
2020-11-25 18:09:08 +0200

    (DB) Update Logs function to save logs in proper location

--- Utils/Logs.php
+++ Utils/Logs.php
@@ -3,5 +3,5 @@
 function logger($msg)
 {
     $msg = date("[d/m] h:i:sa") . ": ". $msg . "\n";
-    file_put_contents('./log.txt', $msg, FILE_APPEND);
+    file_put_contents( $_SERVER['DOCUMENT_ROOT'].'/Utils/Logs/log.txt', $msg, FILE_APPEND);
 }
